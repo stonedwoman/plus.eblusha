51dc3d566a37b61d400aede0303e1eb124b4ea40
Git User <git@example.com>
2025-11-18 18:46:48 +0000

    android: add material theme

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.coil.compose)
+    implementation(libs.android.material)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
