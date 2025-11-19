e28a916b6b92080ed3f483ce876c45e9954852e0
Git User <git@example.com>
2025-11-19 23:17:13 +0000

    android: fix livekit video build

--- android/app/src/main/java/org/eblusha/plus/ui/call/VideoView.kt
+++ android/app/src/main/java/org/eblusha/plus/ui/call/VideoView.kt
@@ -1,5 +1,6 @@
 package org.eblusha.plus.ui.call
 
+import android.graphics.Color
 import android.view.ViewGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -8,7 +9,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import io.livekit.android.room.track.VideoTrack
-import io.livekit.android.video.VideoView
+import livekit.org.webrtc.EglBase
+import livekit.org.webrtc.RendererCommon
+import livekit.org.webrtc.SurfaceViewRenderer
 
 @Composable
 fun LiveKitVideoView(
@@ -16,32 +19,45 @@ fun LiveKitVideoView(
     modifier: Modifier = Modifier,
     mirror: Boolean = false,
 ) {
-    val viewRef = remember { mutableStateOf<VideoView?>(null) }
+    val eglBase = remember { EglBase.create() }
+    val rendererRef = remember { mutableStateOf<SurfaceViewRenderer?>(null) }
 
     AndroidView(
         factory = { context ->
-            VideoView(context).apply {
+            SurfaceViewRenderer(context).apply {
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
+                init(eglBase.eglBaseContext, null)
+                setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
                 setEnableHardwareScaler(true)
-                setMirror(mirror)
-                viewRef.value = this
+                setZOrderMediaOverlay(true)
+                setBackgroundColor(Color.TRANSPARENT)
+                rendererRef.value = this
             }
         },
         modifier = modifier,
         update = { view ->
             view.setMirror(mirror)
-            view.setVideoTrack(track)
+            val previousTrack = view.tag as? VideoTrack
+            if (previousTrack != track) {
+                previousTrack?.removeRenderer(view)
+                track?.addRenderer(view)
+                view.tag = track
+            }
         }
     )
 
-    DisposableEffect(Unit) {
+    DisposableEffect(track) {
         onDispose {
-            viewRef.value?.setVideoTrack(null)
-            viewRef.value?.release()
-            viewRef.value = null
+            rendererRef.value?.let { renderer ->
+                val taggedTrack = renderer.tag as? VideoTrack
+                taggedTrack?.removeRenderer(renderer)
+                renderer.tag = null
+                renderer.release()
+            }
+            eglBase.release()
         }
     }
 }
