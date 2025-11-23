60595f25586ca3c5a6f766f011f6ed0c3ae39d33
Git User <git@example.com>
2025-11-23 15:26:45 +0000

    Fix plugin logging in MainActivity

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -3,6 +3,7 @@ package org.eblusha.plus;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Logger;
+import com.getcapacitor.Bridge;
 import java.util.List;
 import com.getcapacitor.Plugin;
 
@@ -10,13 +11,21 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
         
         // Логируем загруженные плагины для диагностики
         try {
-            List<Class<? extends Plugin>> plugins = getBridge().getPlugins();
-            Logger.debug("Loaded plugins count: " + plugins.size());
-            for (Class<? extends Plugin> plugin : plugins) {
-                Logger.debug("Plugin: " + plugin.getName());
+            Bridge bridge = getBridge();
+            if (bridge != null) {
+                List<Class<? extends Plugin>> plugins = bridge.getPlugins();
+                Logger.debug("Loaded plugins count: " + plugins.size());
+                for (Class<? extends Plugin> plugin : plugins) {
+                    Logger.debug("Plugin: " + plugin.getName());
+                }
             }
         } catch (Exception e) {
             Logger.error("Error logging plugins", e);
