e7dcfe4781a61ac044d88a5f5ca0bb02f987c508
Git User <git@example.com>
2025-11-23 15:28:13 +0000

    Manually register LocalNotificationsPlugin in MainActivity

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -3,32 +3,20 @@ package org.eblusha.plus;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Logger;
-import com.getcapacitor.Bridge;
-import java.util.List;
-import com.getcapacitor.Plugin;
+import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-    
-    @Override
-    protected void onStart() {
-        super.onStart();
         
-        // Логируем загруженные плагины для диагностики
+        // Явная регистрация плагина LocalNotifications
+        // Это гарантирует, что плагин будет зарегистрирован даже если автоматическая регистрация не сработает
         try {
-            Bridge bridge = getBridge();
-            if (bridge != null) {
-                List<Class<? extends Plugin>> plugins = bridge.getPlugins();
-                Logger.debug("Loaded plugins count: " + plugins.size());
-                for (Class<? extends Plugin> plugin : plugins) {
-                    Logger.debug("Plugin: " + plugin.getName());
-                }
-            }
+            registerPlugin(LocalNotificationsPlugin.class);
+            Logger.debug("LocalNotificationsPlugin registered manually");
         } catch (Exception e) {
-            Logger.error("Error logging plugins", e);
+            Logger.error("Failed to register LocalNotificationsPlugin", e);
         }
     }
 }
