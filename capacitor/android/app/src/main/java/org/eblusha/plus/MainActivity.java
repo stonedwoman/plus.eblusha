5379790fd9598c8e1a6d6928087fda74c3bff42e
Git User <git@example.com>
2025-11-23 13:52:56 +0000

    Revert manual plugin registration - use auto-registration in Capacitor 6

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -1,14 +1,5 @@
 package org.eblusha.plus;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Явная регистрация плагина LocalNotifications
-        registerPlugin(LocalNotificationsPlugin.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
