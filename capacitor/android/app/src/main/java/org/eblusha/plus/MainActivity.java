b05738cd64cc5953ecd705be335d8cdc03b888d0
Git User <git@example.com>
2025-11-23 15:38:39 +0000

    Try explicit plugin registration via init() method

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -1,5 +1,25 @@
 package org.eblusha.plus;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Явная регистрация плагина LocalNotifications через init()
+        // Это гарантирует, что плагин будет зарегистрирован даже если автоматическая регистрация не сработает
+        try {
+            Class<? extends Plugin> pluginClass = (Class<? extends Plugin>) Class.forName("com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin");
+            ArrayList<Class<? extends Plugin>> plugins = new ArrayList<>();
+            plugins.add(pluginClass);
+            this.init(savedInstanceState, plugins);
+        } catch (ClassNotFoundException e) {
+            // Если класс не найден, просто используем стандартную инициализацию
+            super.onCreate(savedInstanceState);
+        }
+    }
+}
