7243017d577d80d8ac322093b76ace75904be06b
Git User <git@example.com>
2025-11-23 15:44:13 +0000

    Revert manual LocalNotifications init

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -1,23 +1,5 @@
 package org.eblusha.plus;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Явная регистрация плагина LocalNotifications через init()
-        // Это гарантирует, что плагин будет зарегистрирован даже если автоматическая регистрация не сработает
-        try {
-            Class<? extends Plugin> pluginClass = (Class<? extends Plugin>) Class.forName("com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin");
-            ArrayList<Class<? extends Plugin>> plugins = new ArrayList<>();
-            plugins.add(pluginClass);
-            this.init(savedInstanceState, plugins);
-        } catch (ClassNotFoundException e) {
-            // Если класс не найден, используем стандартную инициализацию
-            super.onCreate(savedInstanceState);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
