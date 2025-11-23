6cd748611f4626b1d3bfa4003d7d5be3765a8d9e
Git User <git@example.com>
2025-11-23 15:26:02 +0000

    Add plugin loading diagnostics to MainActivity

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -1,5 +1,25 @@
 package org.eblusha.plus;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Logger;
+import java.util.List;
+import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Логируем загруженные плагины для диагностики
+        try {
+            List<Class<? extends Plugin>> plugins = getBridge().getPlugins();
+            Logger.debug("Loaded plugins count: " + plugins.size());
+            for (Class<? extends Plugin> plugin : plugins) {
+                Logger.debug("Plugin: " + plugin.getName());
+            }
+        } catch (Exception e) {
+            Logger.error("Error logging plugins", e);
+        }
+    }
+}
