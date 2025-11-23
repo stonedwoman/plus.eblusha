2e299bd131342cbdae098b1bd0291a138a56bbf1
Git User <git@example.com>
2025-11-23 15:30:29 +0000

    Use reflection to register LocalNotificationsPlugin

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -3,18 +3,21 @@ package org.eblusha.plus;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Logger;
-import com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin;
+import com.getcapacitor.Plugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Явная регистрация плагина LocalNotifications
+        // Явная регистрация плагина LocalNotifications через рефлексию
         // Это гарантирует, что плагин будет зарегистрирован даже если автоматическая регистрация не сработает
         try {
-            registerPlugin(LocalNotificationsPlugin.class);
-            Logger.debug("LocalNotificationsPlugin registered manually");
+            Class<? extends Plugin> pluginClass = (Class<? extends Plugin>) Class.forName("com.capacitorjs.plugins.localnotifications.LocalNotificationsPlugin");
+            registerPlugin(pluginClass);
+            Logger.debug("LocalNotificationsPlugin registered manually via reflection");
+        } catch (ClassNotFoundException e) {
+            Logger.error("LocalNotificationsPlugin class not found", e);
         } catch (Exception e) {
             Logger.error("Failed to register LocalNotificationsPlugin", e);
         }
