d00c48fd1fb8fe6d65b0d8d23a42b60f97be2b86
Git User <git@example.com>
2025-11-24 17:30:37 +0000

    fix: register custom capacitor plugins

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -1,5 +1,13 @@
 package org.eblusha.plus;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        registerPlugin(MessageNotificationPlugin.class);
+        registerPlugin(IncomingCallPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
