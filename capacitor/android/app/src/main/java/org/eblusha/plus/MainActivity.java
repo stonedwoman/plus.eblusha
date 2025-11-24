b99fe79e009446dc92fa1e34b0ec4aebc6716df7
Git User <git@example.com>
2025-11-24 19:34:06 +0000

    Remove duplicate Override annotation

--- capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
+++ capacitor/android/app/src/main/java/org/eblusha/plus/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    @Override
     public void onResume() {
         super.onResume();
         if (pendingCallIntent != null) {
